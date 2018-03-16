b0daed4a45a258c88a54ea7eac7fcdec7a06a058
ha-mrx <33704360+ha-mrx@users.noreply.github.com>
2018-03-17 01:30:33 +0300

    Add files via upload

---
+++ Remote10Access.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#Ten Remout Remote Access Tools Create By HA-MRX
+#YouTube  : https://www.youtube.com/c/HA-MRX
+#Website : https://kurdkali.wordpress.com/
+#Facebook  : https://www.facebook.com/muhamad.jabar222
+#Author : Mehedi Shakeel
+
+clear
+
+cat  <<  EOF 
+██████╗ ███████╗███╗   ███╗ ██████╗ ████████╗███████╗     ██╗ ██████╗      █████╗  ██████╗ ██████╗███████╗███████╗███████╗
+██╔══██╗██╔════╝████╗ ████║██╔═══██╗╚══██╔══╝██╔════╝    ███║██╔═████╗    ██╔══██╗██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝
+██████╔╝█████╗  ██╔████╔██║██║   ██║   ██║   █████╗      ╚██║██║██╔██║    ███████║██║     ██║     █████╗  ███████╗███████╗
+██╔══██╗██╔══╝  ██║╚██╔╝██║██║   ██║   ██║   ██╔══╝       ██║████╔╝██║    ██╔══██║██║     ██║     ██╔══╝  ╚════██║╚════██║
+██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝   ██║   ███████╗     ██║╚██████╔╝    ██║  ██║╚██████╗╚██████╗███████╗███████║███████║
+╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝    ╚═╝   ╚══════╝     ╚═╝ ╚═════╝     ╚═╝  ╚═╝ ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝
+                                                                                                                          
+
+                                                   
+EOF
+
+mkdir Toolkit
+cd Toolkit
+
+# Git Repos...
+
+
+git clone https://github.com/Screetsec/TheFatRat
+git clone https://github.com/Veil-Framework/Veil
+git clone https://github.com/tiagorlampert/CHAOS
+git clone https://github.com/pasahitz/zirikatu
+git clone https://github.com/Mr-Un1k0d3r/DKMC
+git clone https://github.com/rand0m1ze/ezsploit
+git clone https://github.com/M4sc3r4n0/Evil-Droid
+git clone https://github.com/AhMyth/AhMyth-Android-RAT
+git clone https://github.com/dana-at-cp/backdoor-apk
+git clone https://github.com/ha-mrx/routersploit
+
+
+echo FINISH!!! Now ENJOY!!!
+clear
+ls
